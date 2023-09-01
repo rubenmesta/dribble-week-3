b3d0790d244926193c6527c91dadd796dfd9c34b
Ruben Mesta <rubenmesta29@gmail.com>
2023-09-01 12:25:19 -0400

    add button

---
+++ dist/cjs/utils/colors.d.ts
@@ -0,0 +1,38 @@
+export declare const colors: {
+    primary: {
+        light: string;
+        base: string;
+        dark: string;
+        hover: string;
+    };
+    secondary: {
+        light: string;
+        base: string;
+        dark: string;
+        hover: string;
+    };
+    teritary: {
+        light: string;
+        base: string;
+        dark: string;
+        hover: string;
+    };
+    black: {
+        light: string;
+        base: string;
+        dark: string;
+        hover: string;
+    };
+    grey: {
+        light: string;
+        base: string;
+        dark: string;
+        hover: string;
+    };
+    white: {
+        light: string;
+        base: string;
+        dark: string;
+        hover: string;
+    };
+};
---
+++ dist/cjs/utils/colors.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.colors = void 0;
+exports.colors = {
+    primary: {
+        light: '#A1B9CE',
+        base: '#324A60',
+        dark: '##151F28',
+        hover: '##3E5D79',
+    },
+    secondary: {
+        light: '#A1B9CE',
+        base: '#324A60',
+        dark: '##151F28',
+        hover: '##3E5D79',
+    },
+    teritary: {
+        light: '#A1B9CE',
+        base: '#324A60',
+        dark: '##151F28',
+        hover: '##3E5D79',
+    },
+    black: {
+        light: '#A1B9CE',
+        base: '#324A60',
+        dark: '##151F28',
+        hover: '##3E5D79',
+    },
+    grey: {
+        light: '#A1B9CE',
+        base: '#324A60',
+        dark: '##151F28',
+        hover: '##3E5D79',
+    },
+    white: {
+        light: '#A1B9CE',
+        base: '#324A60',
+        dark: '##151F28',
+        hover: '##3E5D79',
+    },
+};
+//# sourceMappingURL=colors.js.map
